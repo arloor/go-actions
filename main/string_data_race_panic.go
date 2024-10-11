d1328be9b7898cf2e38065c5fcc8fe3b63b308e1
arloor <admin@arloor.com>
2024-10-11 10:53:19 +0800

    Fix data race panic when concurrently reading and writing a string

--- main/string_data_race_panic.go
+++ main/string_data_race_panic.go
@@ -10,13 +10,13 @@ import (
 // 并发读写string，会panic
 func main() {
 	fullPath := "init"
-	go func() {
+	go func() { // goroutine 不断读取fullpath
 		for i := 1; i < 10000; i++ {
 			request(fullPath)
 		}
 	}()
 
-	for {
+	for { // main goroutine会不断修改fullPath.
 		fullPath = ""
 		time.Sleep(10 * time.Nanosecond)
 		fullPath = "/test/test/test"
@@ -25,8 +25,8 @@ func main() {
 
 }
 
-func request(c string) {
+func request(c string) { // 这里传参，有一次拷贝，会做feild（string的poiner和len）的赋值
 	_, _ = json.Marshal(c)
-	// 或者下面的读取
+	// 或者下面的读取，也会panic
 	// println(fmt.Sprintf("fullPath: %s", c))
 }
